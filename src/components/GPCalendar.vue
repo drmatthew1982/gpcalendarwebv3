760f60a7ddbffed564b1c5933f4aac8c3cf7eb17
Matthew Zhu <drmatthew1982@163.com>
2023-06-25 18:58:00 +1200

    event load function

--- src/components/GPCalendar.vue
+++ src/components/GPCalendar.vue
@@ -9,21 +9,24 @@ import UpdateEventDialog from '@/components/Event/UpdateEventDialog.vue'
 import CreateEventDialog from '@/components/Event/CreateEventDialog.vue'
 import { ElMessage, ElMessageBox } from 'element-plus'
 import type { Action } from 'element-plus'
+import service from "@/webservice";
 
 
 let updateDialogShow = ref(false);
 let createDialogShow = ref(false);
 let defaultDate = ref();
+let calendar = ref();
 const dialogClosed = ()=>{
     console.log("dialogClosed");
     updateDialogShow.value = false;
     createDialogShow.value = false;
+    findEvent(calendar.view.currentEnd.toISOString(),calendar);
 }
-
+let events=[];
 document.addEventListener('DOMContentLoaded', function() {
     let calendarEl:HTMLElement = document.getElementById('calendar')!;
-    console.log(document.getElementById('333'));
-    const calendar = new Calendar(calendarEl, {
+    //console.log(document.getElementById('333'));
+    calendar = new Calendar(calendarEl, {
         plugins: [ dayGridPlugin,timeGridPlugin,interactionPlugin ],
         initialView: 'dayGridMonth',
         headerToolbar: {
@@ -37,20 +40,54 @@ document.addEventListener('DOMContentLoaded', function() {
             console.log(createDialogShow.value);
             createDialogShow.value=true;
             defaultDate.value=info.dateStr;
+
         },
         eventClick: function(info) {
             console.log(updateDialogShow.value);
             updateDialogShow.value=true;
             //emit('eventClick',info.event.id);
-        },
-        events:[
-            {id:'111', title: 'event 1', start: '2023-06-04T10:00:00',end:'2023-06-04T12:00:00'},
-            {id:'222', title: 'event 2', start: '2023-06-04T11:00:00',end:'2023-06-04T12:30:00'},
-            {id:'333', title: 'event 3', start: '2023-06-04T11:00:00',end:'2023-06-04T12:30:00',assign:'matthew',editable:false,backgroundColor:'#555555'}
-        ]
+        }
+        // events:
+        //     [
+        //     {id:'111', title: 'event 1', start: '2023-06-04T10:00:00',end:'2023-06-04T12:00:00'},
+        //     {id:'222', title: 'event 2', start: '2023-06-04T11:00:00',end:'2023-06-04T12:30:00'},
+        //     {id:'333', title: 'event 3', start: '2023-06-04T11:00:00',end:'2023-06-04T12:30:00',assign:'matthew',editable:false,backgroundColor:'#555555'}
+        // ]
     });
+
+    console.log(calendar.view.currentStart);
+    //calendar.addEventSource(events);
+    findEvent(calendar.view.currentEnd.toISOString(),calendar);
     calendar.render();
+    console.log(calendar.getEvents());
 });
+
+const headers = {
+    Accept: 'application/json;charset=UTF-8',
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
+const findEvent = (current_date,calendar) => {
+    console.log(current_date);
+    let par = {
+        "user_id": localStorage.getItem('userid'),
+        "current_date":current_date.split('T')[0]
+    }
+    service.get('http://localhost:8080/findeventsbyuserid',
+        par,
+        headers)
+        .then(response => {
+            //console.log(response.data);
+            events = response.data;
+            calendar.removeAllEvents();
+            // events.forEach(function(value,index,array){
+            //     calendar.addEvent(value);
+            // })
+            calendar.addEventSource(events);
+            console.log(events);
+            console.log(calendar.getEvents());
+
+        })
+}
 
 </script>
 <template>
